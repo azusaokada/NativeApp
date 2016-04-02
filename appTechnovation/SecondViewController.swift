05d7a31cf55b672d5cb12bb679ddf614d9c73c30
azusaokada <okada.azusa@gmail.com>
2016-04-01 20:26:11 -0400

    Initial Commit

---
+++ appTechnovation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  appTechnovation
+//
+//  Created by Azusa Okada-17 on 4/1/16.
+//  Copyright © 2016 Azusa Okada-17. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
